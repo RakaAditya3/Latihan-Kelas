11e5bff9d27a942045b9d80b8683cf0cb2b94fb3
Rakaa <77088983+RakaAditya3@users.noreply.github.com>
2024-08-21 13:54:10 +0700

    Add files via upload

---
+++ WebSekolah/content.php
@@ -0,0 +1,12 @@
+<?php
+    $sekolah = "Web SMPKN 2 Buduran";
+    $judul = "SMKN 2 Buduran";
+    $isi = "Ini Adalah Web Sekolah SMKN 2 Buduran yang dibuat oleh siswa kelas XI-RPL 2024
+            atas nama Raka Aditya tanggal 15 Agustus 1945";
+    $hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat"];
+    $pelajaran = ["Matematika", "B.Indo", "B.Jawa", "RPL", "B.Jepang", "Agama", "B.Inggris", "PJOK",
+                    "B.Jerman", "PKN", "PKWU", "Sejarah", "BK"];
+    $jadwal = "jadwal Pelajaran RPL tahun 20200/202500";
+    $gambar = ["wolf1.jpeg", "wolf2.jpeg", "wolf3.jpeg"];
+?>
+
